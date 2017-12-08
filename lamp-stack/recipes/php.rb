8779f6018ba4d1e6534d59fa27997cfe62d69785
arun.tewatia <arun.tewatia@cliffex.com>
2017-12-08 16:20:56 +0530

    Updated php.rb

--- lamp-stack/recipes/php.rb
+++ lamp-stack/recipes/php.rb
@@ -2,6 +2,10 @@ package "php7.0" do
   action :install
 end
 
+package "libapache2-mod-php7.0" do
+  action :install
+end
+
 package "php-pear" do
   action :install
 end
@@ -19,9 +23,3 @@ directory "/var/log/php" do
   action :create
   notifies :run, "execute[chownlog]"
 end
-
-execute "libapache2" do
-  command "apt-get libapache2-mod-php7.0 -y"
-  action :run
-end
-~     
