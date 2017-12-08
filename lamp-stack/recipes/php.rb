b9d778d19f7630ddfdb9394a4c690dd12d2afa6b
arun.tewatia <arun.tewatia@cliffex.com>
2017-12-08 16:12:33 +0530

    Added php.rb

---
+++ lamp-stack/recipes/php.rb
@@ -0,0 +1,21 @@
+package "php7.0" do
+  action :install
+end
+
+package "php-pear" do
+  action :install
+end
+
+package "php-mysql" do
+  action :install
+end
+
+execute "chownlog" do
+  command "chown www-data /var/log/php"
+  action :nothing
+end
+
+directory "/var/log/php" do
+  action :create
+  notifies :run, "execute[chownlog]"
+end
